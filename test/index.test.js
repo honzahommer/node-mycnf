5975b2587d8740e1d35a9add6ad114779fd84927
Honza Hommer <honza@hommer.cz>
2019-02-10 12:56:55 +0100

    Update tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,6 @@
 const should = require('should');
 const lib = require('../lib');
+const { join } = require('path');
 
 describe('mycnf', () => {
   it('returns module', () => {
@@ -8,5 +9,9 @@ describe('mycnf', () => {
 
   it('returns object', () => {
     should(lib()).be.an.instanceOf(Object);
+  });
+
+  it('returns database property', () => {
+    should(lib({ file: join(__dirname, '.my.cnf') })).have.property('database', 'test');
   });
 });
